1845c994673b0f6aebb5ee0028b5964841398a50
RIZET <ryzenupp@hotmail.com>
2020-10-03 08:54:32 -0400

    converted to cmake

---
+++ cmake-build-debug/CMakeFiles/bhlac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bhlac.dir/main.cpp.o"
+  "bhlac"
+  "bhlac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bhlac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
